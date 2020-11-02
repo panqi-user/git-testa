e96287a7fe555434e89536b1814fb04f85b43be1
panqi <panqi@zhidetec.com>
2020-11-02 20:45:26 +0800

    运行mian方法

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -2,7 +2,9 @@ package com.itheima.git;
 
 public class FirstClass {
 
-
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
 
     public void test(){
         int i =0;
